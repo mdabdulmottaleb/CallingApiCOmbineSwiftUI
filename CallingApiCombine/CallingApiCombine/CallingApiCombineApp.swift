62d434298ad3400ad8e4503a91d0c04ddaba14b9
MdAbdulMottaleb <61162466+mdabdulmottaleb@users.noreply.github.com>
2022-03-10 09:15:27 +0600

    Add files via upload

---
+++ CallingApiCombine/CallingApiCombine/CallingApiCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallingApiCombineApp.swift
+//  CallingApiCombine
+//
+//  Created by MacBook Pro on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CallingApiCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CallingApiCombine/CallingApiCombine/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  CallingApiCombine
+//
+//  Created by MacBook Pro on 10/2/22.
+//
+
+import SwiftUI
+import Combine
+
+struct PostModel: Identifiable, Codable {
+    
+    let userId: Int
+    let id : Int
+    let title: String
+    let body: String
+}
+
+class DownloadWithCombineViewModel: ObservableObject{
+    
+    @Published var posts: [PostModel] = []
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        
+        getPost()
+        
+    }
+    
+    func getPost() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            //.subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .tryMap(handleOutput)
+            .decode(type: [PostModel].self, decoder: JSONDecoder())
+            //.replaceError(with: T##[PostModel])
+            .sink { (completion) in
+//                switch completion {
+//                case .finished: print("Finished!")
+//                case .failure(let error): print("There was an error \(error)")
+//                }
+//                print("COMPLETION: \(completion)")
+            } receiveValue: { [weak self] (returnedPost) in
+                self?.posts = returnedPost
+            }
+            .store(in: &cancellables)
+    }
+    
+    func handleOutput(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        
+        guard
+            let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
+            
+            throw URLError(.badServerResponse)
+        }
+        return output.data
+    }
+}
+
+struct ContentView: View {
+    
+    @StateObject var vm = DownloadWithCombineViewModel()
+    
+    var body: some View {
+        List {
+            ForEach(vm.posts) { post in
+                VStack(alignment: .leading){
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
